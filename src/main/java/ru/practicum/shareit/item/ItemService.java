5b855f3c2e245cc10984b83cd62e527d0b6f0272
juzittt <juzittt@yandex.ru>
2026-05-05 14:14:49 +0300

    Feat: Added loggers in service layer

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -21,14 +21,16 @@ public class ItemService {
     private final UserService userService;
 
     public ItemDto addItem(Long userId, ItemDto itemDto) {
+        log.debug("[AddItem] Adding a new item '{}', by user with id: {}", itemDto.getName(), userId);
         Item item = itemMapper.toEntity(itemDto);
+        log.debug("[AddItem] Setting user with id: {}, for item '{}'", userId, itemDto.getName());
         item.setOwner(userService.getUser(userId));
         itemRepository.addItem(item);
         return getItemById(item.getItemId());
     }
 
     public ItemDto updateItem(Long userId, Long itemId, UpdateItemRequest request) {
-        log.info("Updating item with id={}", itemId);
+        log.debug("[UpdateItem] Updating item with id: {}", itemId);
         Item item = validateOwner(userId, itemId);
         itemMapper.updateItemFromRequest(item, request);
         Item updatedItem = itemRepository.updateItem(item);
@@ -36,35 +38,40 @@ public class ItemService {
     }
 
     public List<ItemDto> getItems(Long userId) {
+        log.debug("[GetItems] Getting all user items");
         return itemRepository.getItems(userId);
     }
 
     public ItemDto getItemById(Long id) {
+        log.debug("[GetItemById] Getting item with id: {}", id);
         return itemMapper.toDto(getItemEntity(id));
     }
 
     public Item getItemEntity(Long id) {
         return itemRepository.getItem(id)
                 .orElseThrow(() -> {
-                    log.warn("Item with id={} not found", id);
+                    log.warn("[getItemEntity - Error] Item with id:{} not found", id);
                     return new NotFoundException("Предмет с id = " + id + " не найден.");
                 });
     }
 
     public List<ItemDto> searchItem(Long userId, String text) {
+        log.debug("[SearchItem] Searching items whose owner`s id: {}, contains text: '{}'", userId, text);
         if (text == null || text.isEmpty()) {
+            log.debug("[SearchItem] Text to find in name or description was empty");
             return List.of();
         }
         return itemMapper.toDtoList(itemRepository.searchItem(userId, text));
     }
 
     private Item validateOwner(Long userId, Long itemId) {
+        log.debug("[ValidateOwner] Validating owner id: {} for item with id: {}", userId, itemId);
         User user = userService.getUser(userId);
         Item entity = itemRepository.getItem(itemId)
                 .filter(item -> item.getOwner() != null
                         && item.getOwner().getUserId().equals(user.getUserId()))
                 .orElseThrow(() -> {
-                    log.warn("Item with id={} which owner id is {} doesn`t exist", itemId, userId);
+                    log.warn("[validateOwner - Error] Item with id: {} which owner id is {} doesn`t exist", itemId, userId);
                     return new NotFoundException("Предмет с id = " + itemId +
                             ", где владелец c id=" + userId + " не найден.");
                 });
--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -18,14 +18,16 @@ public class UserService {
     private final UserMapper userMapper;
 
     public User getUser(long id) {
+        log.debug("[GetUser] Getting user with id: {}", id);
         return userRepository.getUserById(id)
                 .orElseThrow(() -> {
-                    log.warn("User with id {} doesn`t exists", id);
+                    log.warn("[GetUser - Error] User with id {} doesn`t exists", id);
                     return new NotFoundException("Пользователь с id " + id + " не найден.");
                 });
     }
 
     public UserDto createUser(@Valid NewUserRequest request) {
+        log.debug("[CreateUser] Create new user with name: {} and email: {}", request.getName(), request.getEmail());
         validateEmail(request.getEmail());
 
         User user = userMapper.toEntity(request);
@@ -33,6 +35,8 @@ public class UserService {
     }
 
     public UserDto updateUser(Long userId, UpdateUserRequest request) {
+        log.debug("[UpdateUser] Update for user with id: {}. Fields from request: name: {}, email: {}",
+                userId, request.getName(), request.getEmail());
         User updateUser = getUser(userId);
 
         request.setId(userId);
@@ -49,26 +53,32 @@ public class UserService {
     }
 
     public void deleteUser(Long userId) {
+        log.debug("[DeleteUser] delete user with id: {}", userId);
         getUser(userId);
         userRepository.deleteUserById(userId);
     }
 
     private void validateEmail(String email) {
+        log.debug("[ValidateEmail] Validate email: {}", email);
         if (userRepository.isEmailExists(email)) {
-            log.warn("Email {} already exists", email);
+            log.warn("[ValidateEmail - Error] Email {} already exists", email);
            throw new DuplicatedDataException("Email: " + email + " уже существует.");
         }
     }
 
 
     private void updateIfNotBlank(UpdateUserRequest request, User user) {
+        log.debug("[UpdateFieldsIfNotBlanks] Updating fields");
         if (request.getName() != null && !request.getName().isBlank()) {
+            log.debug("[UpdateFieldsIfNotBlanks - Name] Update name from '{}' to '{}'", user.getName(), request.getName());
             user.setName(request.getName());
         }
 
         String newEmail = request.getEmail();
         if (newEmail != null && !newEmail.isBlank()) {
+            log.debug("[UpdateFieldsIfNotBlanks - Email] Update email from '{}' to '{}'", user.getEmail(), newEmail);
             if (!newEmail.equals(user.getEmail()) && userRepository.isEmailExists(newEmail)) {
+                log.warn("[UpdateFieldsIfNotBlanks - Email - Error] Email is already exists");
                 throw new DuplicatedDataException("Email: " + newEmail + " уже существует.");
             }
             user.setEmail(newEmail);
