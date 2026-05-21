84a160b8fea4d9c17d23b0ab8dbe6a6db5aa4552
juzittt <juzittt@yandex.ru>
2026-05-21 11:15:49 +0300

    FEAT: add booking, comment. Upd: Rebuild repositories with usage JPA

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -2,13 +2,12 @@ package ru.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class ShareItApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShareItApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShareItApp.class, args);
+    }
 
 }
--- src/main/java/ru/practicum/shareit/booking/BookingStatus.java
+++ src/main/java/ru/practicum/shareit/booking/BookingStatus.java
@@ -1,7 +1,5 @@
 package ru.practicum.shareit.booking;
 
-import lombok.Data;
-
 public enum BookingStatus {
-    WAITING, APPROVED, REJECTED, CANCELED;
+    WAITING, APPROVED, REJECTED, CANCELED
 }
